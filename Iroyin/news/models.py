535957db14d647610fb2c5b9cb2c87c66a5e265f
Jeremiah Lena <jeremiahlena13@gmail.com>
2022-01-16 13:13:03 +0100

    somewhere reasonable

--- Iroyin/news/models.py
+++ Iroyin/news/models.py
@@ -1,6 +1,25 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
 class News(models.Model):
     title = models.CharField(max_length=500)
-    url = models.URLField()
+    url = models.URLField(unique=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def serialize(self):
+        
+        return {'newsId': self.id, 'title': self.title, 'url': self.url }
+    
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.TextField(max_length=50)
+    newsSeen = models.ManyToManyField(to = News, related_name='readers')
+    newsIntereactedWith = models.ManyToManyField(to = News, related_name='readers_interacted')
+    
+    def __str__(self):
+        return self.username
+    
--- Iroyin/news/views.py
+++ Iroyin/news/views.py
@@ -1,7 +1,65 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, Http404
+from .scraper import PunchScraper
+import json
+from .models import News, User
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<H1>Welcome here </H1>')
+    
+    try:
+        
+        me = User.objects.get(username = 'jeremiah')
+        
+        punchScraper = PunchScraper('politics')
+        
+        
+        data = punchScraper.scrape()
+                
+        newsSeen = [news.serialize() for news in me.newsSeen.all()]
+        newsInteracted = [news.serialize() for news in me.newsIntereactedWith.all()]
+    
+        ids, titles, urls,interactions=[],[],[],[]
+        
+        for i in range(len(newsSeen)):
+            #print(newsSeen[i])
+            ids.append(newsSeen[i]['newsId'])
+            titles.append(newsSeen[i]['title'])
+            urls.append(newsSeen[i]['url'])
+            
+            if newsSeen[i] in newsInteracted:
+                interactions.append(1)
+            else:
+                interactions.append(0)
+        
+        trainingData = {'ids': ids, 'titles': titles, 'urls': urls, 'interactions': interactions}
+        print('trainging data:', trainingData)
+        
+        # data = model.predict(trainingData, dataFromScraper)
+        
+        for news in data:
+            try:
+                existing_news = get_object_or_404(News, url = news['url'])
+                me.newsSeen.add(existing_news)
+            except Http404:
+                me.newsSeen.add(News.objects.create(title = news['headline'], url = news['url']))
+                
+        me.save()
+            
+        
+    except Exception as e:
+        print(e)
+        pass
+        
+    
+    return JsonResponse(json.dumps(data), safe=False)
+
+
+def prepareDataForModel(user):
+    newsSeen = list(user.newsSeen.all())
+    newInteractedWith = list(user.newsIntereactedWith.all())
+
+    # for news in newsSeen:
+        # if(+)
