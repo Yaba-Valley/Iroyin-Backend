4d1fd0d92b3984753ba6913f3aabbb2b1ba34e4f
Jeremiah Lena <jeremiahlena13@gmail.com>
2022-01-24 15:49:10 +0100

    added allowed hosts

--- Iroyin/settings.py
+++ Iroyin/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6329=4&y$@285u+xi$p)h=_o08fti+cljm9mct$ks(%*j+vv40'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
