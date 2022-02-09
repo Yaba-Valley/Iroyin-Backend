d9ceff0f275b1af5f2e7f169e72959d8c49a5d85
Jeremiah Lena <jeremiahlena13@gmail.com>
2022-02-09 21:06:43 +0100

    fixed error

--- news/recommend.py
+++ news/recommend.py
@@ -1,3 +1,4 @@
+from code import interact
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import Pipeline
 import pandas as pd
@@ -10,36 +11,69 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score, f1_score
 import nltk
-### dL
+import os
+from sqlalchemy import create_engine
+
+
+def get_df(id):
+    print('running')
+    path = os.getcwd()+'/db.sqlite3'
+
+    engine = create_engine('sqlite:///'+path)
+    query = f""" SELECT news_news.title         
+    FROM news_news
+    INNER JOIN news_user_newInteractedWith        
+    ON news_news.id=news_user_newInteractedWith.news_id
+    WHERE news_user_newInteractedWith.user_id={id}; """
+
+    interacted = pd.read_sql_query(query, engine)
+    print(interacted)
+    
+    interacted['interactions'] =  [1] * len(interacted)
+    print('After insertion:\n\n')
+    
+    query = f""" SELECT news_news.title           
+    FROM news_news
+    INNER JOIN news_user_newsSeen        
+    ON news_news.id=news_user_newsSeen.news_id
+    WHERE news_user_newsSeen.user_id={id}; """
+
+    seen = pd.read_sql(query, engine)
+    # print(seen)
+
+
+get_df(1)
+# dL
 #from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer
 
 
-
 def clean_text(text, flg_stemm=False, flg_lemm=False, lst_stopwords=None):
-    ## clean (convert to lowercase and remove punctuations and   
-    ##characters and then strip)
-    text= re.sub(r'^[0-9]|[0-9]$|\w+(-\w+)', 'GoalScoredMatch', str(text).lower().strip())
+    # clean (convert to lowercase and remove punctuations and
+    # characters and then strip)
+    text = re.sub(r'^[0-9]|[0-9]$|\w+(-\w+)',
+                  'GoalScoredMatch', str(text).lower().strip())
     text = re.sub(r'[^\w\s]', '', str(text).lower().strip())
-            
-    ## Tokenize (convert from string to list)
-    lst_text = text.split()    ## remove Stopwords
+
+    # Tokenize (convert from string to list)
+    lst_text = text.split()  # remove Stopwords
     if lst_stopwords is not None:
-        lst_text = [word for word in lst_text if word not in 
+        lst_text = [word for word in lst_text if word not in
                     lst_stopwords]
-                
+
     ## Stemming (remove -ing, -ly, ...)
     if flg_stemm == True:
         ps = nltk.stem.porter.PorterStemmer()
         lst_text = [ps.stem(word) for word in lst_text]
-                
-    ## Lemmatisation (convert the word into root word)
+
+    # Lemmatisation (convert the word into root word)
     if flg_lemm == True:
         lem = nltk.stem.wordnet.WordNetLemmatizer()
         lst_text = [lem.lemmatize(word) for word in lst_text]
-            
-    ## back to string from list
+
+    # back to string from list
     text = " ".join(lst_text)
     return text
+
 
 """
  def objective(trial):
@@ -57,52 +91,58 @@ def clean_text(text, flg_stemm=False, flg_lemm=False, lst_stopwords=None):
     return mean_squared_error(Y_test, regressor.predict(X_test))
 
 """
- 
+
+
 class Machine:
     def __init__(self, data):
-        
+
         self.data = data
 
-        self.model= Pipeline(steps=[('tfid-vectorizer', TfidfVectorizer(lowercase = True)), ('feature-selection', SelectPercentile(score_func= f_classif)), ('classifier', RandomForestClassifier(random_state=0))])
+        self.model = Pipeline(steps=[('tfid-vectorizer', TfidfVectorizer(lowercase=True)), ('feature-selection',
+                              SelectPercentile(score_func=f_classif)), ('classifier', RandomForestClassifier(random_state=0))])
 
     def recommend(self, data):
-        scrape= pd.DataFrame(data)
+        scrape = pd.DataFrame(data)
         try:
-            self.data= pd.DataFrame(self.data)
+            get_df(1)
+            self.data = pd.DataFrame(self.data)
             #self.data.to_csv('news.csv', index= False)
-
-            self.data["titles"] = self.data["titles"].apply(lambda x: clean_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=None))
+            print(os.path.dirname('tests.py'))
+            self.data["titles"] = self.data["titles"].apply(lambda x: clean_text(
+                x, flg_stemm=False, flg_lemm=True, lst_stopwords=None))
 
             #self.data.to_csv('news.csv', index = False)
-            param_grid = {'feature-selection__percentile': (10,20,30,40,50,60,70,80,90,100)}
+            param_grid = {
+                'feature-selection__percentile': (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)}
 
-            model_grid_search = GridSearchCV(self.model, 
-                                param_grid=param_grid,
-                                n_jobs=2, 
-                                cv=StratifiedShuffleSplit(n_splits=3, test_size=0.1),
-                                scoring='f1')
-                                
-            model_grid_search.fit(self.data['titles'], self.data['interactions'])
+            model_grid_search = GridSearchCV(self.model,
+                                             param_grid=param_grid,
+                                             n_jobs=2,
+                                             cv=StratifiedShuffleSplit(
+                                                 n_splits=3, test_size=0.1),
+                                             scoring='f1')
+
+            model_grid_search.fit(
+                self.data['titles'], self.data['interactions'])
             #self.model.fit(self.data['titles'], self.data['interactions'])
 
-            probability=model_grid_search.predict_proba(scrape['titles'].apply(lambda x: clean_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=None)))[:,1]
+            probability = model_grid_search.predict_proba(scrape['titles'].apply(
+                lambda x: clean_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=None)))[:, 1]
 
             print('f1 :', model_grid_search.best_score_)
 
-            print(f"The best set of parameters is: "f"{model_grid_search.best_params_}")
+            print(
+                f"The best set of parameters is: "f"{model_grid_search.best_params_}")
 
-            scrape['probability']= probability
+            scrape['probability'] = probability
 
-            recommended_item= scrape.sort_values(by=['probability'], ascending=False).drop(['probability'], axis=1)
+            recommended_item = scrape.sort_values(
+                by=['probability'], ascending=False).drop(['probability'], axis=1)
 
-            recommended_item= recommended_item.iloc[:20, :]
+            recommended_item = recommended_item.iloc[:20, :]
             print('\n\n\n\n\n')
             #print('ml', recommended_item)
             return recommended_item.to_dict(orient='list')
         except Exception as e:
             print(e)
-            return scrape.sample(frac = 1).iloc[:20, :].to_dict(orient = 'list')
-
-            
-
-        
+            return scrape.sample(frac=1).iloc[:20, :].to_dict(orient='list')
