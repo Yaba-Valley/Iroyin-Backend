88ba9ba3e831fc0c50fc12b8a28f296ca4574027
Jeremiah <Jeremiahlena13@gmail.com>
2022-04-09 17:59:12 +0100

    got news model User working

--- news/models.py
+++ news/models.py
@@ -26,6 +26,7 @@ class UserManager(BaseUserManager):
             email,
             password=password,
         )
+        user.is_active = True
         user.staff = True
         user.save(using=self._db)
         return user
@@ -38,6 +39,7 @@ class UserManager(BaseUserManager):
             email,
             password=password,
         )
+        user.is_active = True
         user.staff = True
         user.admin = True
         user.save(using=self._db)
@@ -74,17 +76,49 @@ class User(AbstractBaseUser):
     
     objects = UserManager()
     
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     staff = models.BooleanField(default=False) 
     admin = models.BooleanField(default=False)
     
-    USERNAME_FIELD = 'email'
-    
+    first_name = models.CharField(max_length=100, verbose_name='First Name')
+    last_name = models.CharField(max_length=100, verbose_name='Last Name')
     newsSeen = models.ManyToManyField(to = News, related_name='readers')
     newInteractedWith = models.ManyToManyField(to = News, related_name='readers_interacted')
     interests = models.ManyToManyField(to = Interest, related_name='users')
     
     
+    USERNAME_FIELD = 'email'
+    
+    def get_full_name(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.get_full_name()
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+    
+   
     def __str__(self):
         return self.email
     
