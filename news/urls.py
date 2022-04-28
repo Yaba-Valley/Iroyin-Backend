0bff03d9a59af23c24d19fbc468835085b3ea382
Jeremiah <Jeremiahlena13@gmail.com>
2022-04-28 10:50:19 +0100

    protecting routes 101

--- news/urls.py
+++ news/urls.py
@@ -7,19 +7,19 @@ urlpatterns = [
     # AUTH URLS
     path('auth/login/', views.login, name='login'),
     path('auth/register/', views.register, name='register'),
-    path('auth/activate-account/<str:uid>/<str:token>', views.activate_account, name = 'activate account'),
+    path('auth/activate-account/<str:uid>/<str:token>/', views.Activate_Account.as_view(), name = 'activate account'),
     
     # USER URLS
-    # path('user/interests/', views.user_interest, name = 'user interests'),
-    path('user/interests/add', views.save_interests, name = 'save user interests'),
-    path('user/interests/remove', views.remove_interests, name = 'remove user interests'),
+    path('user/interests/', views.UserInterests.as_view(), name = 'user interests'),
+    path('user/interests/add/', views.save_interests, name = 'save user interests'),
+    path('user/interests/remove/', views.remove_interests, name = 'remove user interests'),
     
     #NEWS URLS
-    path('news/get_news/', views.index, name='index route'),
-    path('news/indicate_interaction/', views.indicate_interaction, name='indicate news interaction'),
+    path('news/get_news/', views.GetNews.as_view(), name='index route'),
+    path('news/indicate_interaction/', views.Indicate_Interaction.as_view(), name='indicate news interaction'),
     
     #STATICS
-    path('interests/all', views.get_all_interests, name = 'get all interests'),
+    path('interests/all/', views.get_all_interests, name = 'get all interests'),
     
     path('test-jwt/', views.HelloView.as_view(), name = 'hello'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name = 'get jwt token')
--- news/views.py
+++ news/views.py
@@ -19,61 +19,69 @@ from .recommend import Machine
 from .utils import fetch_news_async, prepareDataForModel, get_scrapers_based_on_user_interest, send_email, TokenGenerator
 
 
-def index(request):
+class GetNews(APIView):
 
-    try:
-        me = User.objects.get(email='jeremiahlena13@gmail.com')
-        print(me)
+    permission_classes = (IsAuthenticated, )
 
-        data = []
+    def get(self, request):
 
-        scrapers = get_scrapers_based_on_user_interest(me)
+        try:
 
-        data = asyncio.run(fetch_news_async(scrapers, data))
+            data = []
+            me = request.user
 
-        data_to_predict_with = prepareDataForModel(
-            data=data, newsInteracted=None)
+            scrapers = get_scrapers_based_on_user_interest(me)
 
-        recommend_news = Machine(1).recommend(data_to_predict_with)
+            data = asyncio.run(fetch_news_async(scrapers, data))
 
-        for i in range(len(recommend_news['titles'])):
-            try:
-                existing_news = get_object_or_404(
-                    News, url=recommend_news['urls'][i])
-                me.newsSeen.add(existing_news)
-            except Http404:
-                me.newsSeen.add(News.objects.create(
-                    title=recommend_news['titles'][i], url=recommend_news['urls'][i]))
+            data_to_predict_with = prepareDataForModel(
+                data=data, newsInteracted=None)
 
-        me.save()
+            recommend_news = Machine(1).recommend(data_to_predict_with)
 
-        news_for_frontend = []
-        # restructure news for FE
-        for i in range(len(recommend_news['titles'])):
-            news_for_frontend.append({'title': recommend_news['titles'][i], 'url': recommend_news['urls'][i], 'img': recommend_news['imgs'][i], 'metadata': {
-                                      'website': recommend_news['meta'][i]['website'], 'favicon': recommend_news['meta'][i]['favicon']}})
+            news_for_frontend = []
 
-        return JsonResponse({'news': news_for_frontend})
+            for i in range(len(recommend_news['titles'])):
+                try:
+                    #restructure the news recommend for the frontend
+                    news_for_frontend.append({'title': recommend_news['titles'][i], 'url': recommend_news['urls'][i], 'img': recommend_news['imgs'][i], 'metadata': {
+                        'website': recommend_news['meta'][i]['website'], 'favicon': recommend_news['meta'][i]['favicon']}})
+                    
+                    # check to see if news exists with this url
+                    existing_news = get_object_or_404(
+                        News, url=recommend_news['urls'][i])
+                    
+                    # add the news to the user's seen news if it already exists
+                    me.newsSeen.add(existing_news)
+                except Http404:
+                    
+                    # create a new News object and addit to the user's seen news
+                    me.newsSeen.add(News.objects.create(
+                        title=recommend_news['titles'][i], url=recommend_news['urls'][i]))
 
-    except Exception as e:
-        print(e)
-        return HttpResponse(f'<h1>THere is an error <hr /> {e}</h1>')
+            me.save()
+
+            return JsonResponse({'news': news_for_frontend})
+
+        except Exception as e:
+            print(e)
+            return HttpResponse(f'<h1>THere is an error <hr /> {e}</h1>')
 
 
-@csrf_exempt
-def indicate_interaction(request):
+class Indicate_Interaction(APIView):
+    
+    def post(self, request):
+        request_body_unicode = request.body.decode('utf-8')
+        request_body = json.loads(request_body_unicode)
+        news_url = request_body['news_url']
 
-    request_body_unicode = request.body.decode('utf-8')
-    request_body = json.loads(request_body_unicode)
-    news_url = request_body['news_url']
-
-    try:
-        active_user = get_object_or_404(User, username='jeremiah')
-        current_news = get_object_or_404(News, url=news_url)
-        active_user.newInteractedWith.add(current_news)
-    except Http404:
-        return JsonResponse({'message': f'News with url {news_url} does not exist', 'success': False})
-    return JsonResponse({'message': 'Interaction has been recorded', 'success': True})
+        try:
+            active_user = request.user
+            current_news = get_object_or_404(News, url=news_url)
+            active_user.newInteractedWith.add(current_news)
+        except Http404:
+            return JsonResponse({'message': f'News with url {news_url} does not exist', 'success': False})
+        return JsonResponse({'message': 'Interaction has been recorded', 'success': True})
 
 
 @csrf_exempt
@@ -95,7 +103,6 @@ def login(request):
                     site = get_current_site(request).__str__()
                     res = requests.post(
                         f"http://{site}/api/token/", {'email': email, 'password': password})
-                    
 
                     # if the token was generated successfully
                     if res.status_code == 200:
@@ -113,16 +120,16 @@ def login(request):
                     else:
 
                         html_string = TokenGenerator().send_account_activation_mail(request, user)
-                        
+
                         # failure to generate token is mostly a result of user not been activated
                         # return JsonResponse(
-                            # {
-                                # 'success': False,
-                                # 'message': 'Your account has not been activated',
-                                # 'errors': []
-                            # }
+                        # {
+                        # 'success': False,
+                        # 'message': 'Your account has not been activated',
+                        # 'errors': []
+                        # }
                         # )
-                        
+
                         return HttpResponse(html_string)
 
                 else:
@@ -195,9 +202,9 @@ def register(request):
 
                 res = send_email('Heeyyyy, We\'re sooo happy to have you here😊🎉🎉',
                                  template_string, email, f"{first_name} {last_name}")
-                
+
                 TokenGenerator().send_account_activation_mail(request, test_user)
-                
+
                 return JsonResponse({
                     'success': True,
                     'message': 'Account has been created successfully. Check your mail to activate your account',
@@ -209,7 +216,7 @@ def register(request):
             except ValidationError as errors:
                 validation_errors = []
 
-                [ validation_errors.append(str(error)) for error in errors ]
+                [validation_errors.append(str(error)) for error in errors]
 
                 return JsonResponse({'success': False, 'message': 'Please review your password', 'errors': validation_errors}, status=400)
             except IntegrityError:
@@ -233,41 +240,40 @@ def register(request):
         return JsonResponse({'success': False, 'message': "Request Not Allowed"}, status=405)
 
 
-@csrf_exempt
-def activate_account(request, uid, token):
+class Activate_Account(APIView):
     
-    try:
-        user_id = force_str(urlsafe_base64_decode(uid))
-        user_id = int(user_id)
+    def get(self, request,  uid, token):
         
-        user = User.objects.get(id = user_id)
-        
-        is_valid_token = TokenGenerator().check_token(user = user, token = token)
-        
-        if is_valid_token:
-            print('Token is Valid')
-            user.is_active = True
-            user.save()
-        else:
-            return HttpResponse('Invalid token')
-        
-        return HttpResponse('hi ' + user.first_name + '. You can now log in normally')
-        
-    except User.DoesNotExist:
-        return HttpResponse("Guy how far, this person no dey our databse")
-    except DjangoUnicodeDecodeError:
-        return HttpResponse('I catch you, couldn\'t decode this unicode')
+        try:
+            user_id = force_str(urlsafe_base64_decode(uid))
+            user_id = int(user_id)
+
+            user = User.objects.get(id=user_id)
+
+            is_valid_token = TokenGenerator().check_token(user=user, token=token)
+
+            if is_valid_token:
+                print('Token is Valid')
+                user.is_active = True
+                user.save()
+            else:
+                return HttpResponse('Invalid token')
+
+            return HttpResponse('hi ' + user.first_name + '. You can now log in normally')
+
+        except User.DoesNotExist:
+            return HttpResponse("Guy how far, this person no dey our databse")
+        except DjangoUnicodeDecodeError:
+            return HttpResponse('I catch you, couldn\'t decode this unicode')
 
     # user = User.objects.get(email='jeremiahlena13@gmail.com')
 
     # template_string = render_to_string(
         # 'welcomeEmail.html', {'registered_user': user})
     # res = send_email('Heeyyyy, We\'re sooo happy to have you here😊🎉🎉',
-                    #  template_string, user.email, user.first_name)
+        #  template_string, user.email, user.first_name)
 
     # return JsonResponse({'message': 'tried seending mail'}, status=res.status_code)
-    
-    
 
 
 def get_all_interests(request):
@@ -323,9 +329,21 @@ def remove_interests(request):
         return JsonResponse({'success': False, 'errors': 'Request Not Allowed'}, status=405)
 
 
+class UserInterests(APIView):
+
+    permssion_classes = [ IsAuthenticated ]
+        
+    def get(self, request):
+        me = request.user
+        interests = [ {'name': interest.name, 'id': interest.id } for interest in me.interests.all() ]
+        return Response({'success': True, 'interests': interests, 'message': 'Successfully retrieved user interest'})
+        
+        
+
 class HelloView(APIView):
     permission_classes = (IsAuthenticated, )
 
     def get(self, request):
-        content = {'message': 'Hello, GeeksforGeeks', 'username': request.user.email}
+        content = {'message': 'Hello, GeeksforGeeks',
+                   'username': request.user.email}
         return Response(content)
