164a9f6db4bd124a5a4690ab203ab16a508afd1e
Jeremiah Lena <101596542+jeremiahFI@users.noreply.github.com>
2022-11-02 09:53:28 +0100

    updated the News model

--- news/models.py
+++ news/models.py
@@ -57,6 +57,8 @@ class News(models.Model):
     title = models.CharField(max_length=500)
     url = models.URLField(unique=True)
     img = models.URLField(default = 'https://media.istockphoto.com/vectors/news-vector-id918880270?k=20&m=918880270&s=612x612&w=0&h=bDcgr9jhiRYCPMUhVdLKD5ouIc5daM4qMcaPPapppQI=')
+    read_count = models.BigIntegerField(default = 0)
+    categories = models.ManyToManyField(to = Interest)
 
     def __str__(self):
         return self.title
