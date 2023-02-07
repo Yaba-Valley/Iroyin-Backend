ee12c3dc37b61699f81ea7a85f8d9129931ba0b6
Jeremiah Lena <jeremiahlena13@gmail.com>
2023-02-07 23:04:38 +0100

    mail

--- news/utils.py
+++ news/utils.py
@@ -70,24 +70,24 @@ async def fetch_news_async(scrapers, news=[]):
         email_text = render_to_string(
             'scraperInfo.html', {'failed_scrapers': failed_scrapers, 'time_taken': time_taken})
 
-        # res = send_email(
-        #     f'Scraper Information ({len(scrapers)} ran, {len(failed_scrapers)} failed, {len(scrapers) - len(failed_scrapers)} successful)',
-        #     email_text,
-        #     [
-        #         {
-        #             'email': 'jeremiahlena13@gmail.com',
-        #             'fullName': 'Jeremiah Lena'
-        #         },
-        #         {
-        #             'email': 'ikpeleambroseobinna@gmail.com',
-        #             'fullName': 'Ikepele Ambrose'
-        #         },
-        #         {
-        #             'email': 'odeogberinoluwadamilola@gmail.com',
-        #             'fullName': 'Oluwadamilola Odeogberin'
-        #         }
-        #     ]
-        # )
+        res = send_email(
+            f'Scraper Information ({len(scrapers)} ran, {len(failed_scrapers)} failed, {len(scrapers) - len(failed_scrapers)} successful)',
+            email_text,
+            [
+                {
+                    'email': 'jeremiahlena13@gmail.com',
+                    'fullName': 'Jeremiah Lena'
+                },
+                {
+                    'email': 'ikpeleambroseobinna@gmail.com',
+                    'fullName': 'Ikepele Ambrose'
+                },
+                {
+                    'email': 'odeogberinoluwadamilola@gmail.com',
+                    'fullName': 'Oluwadamilola Odeogberin'
+                }
+            ]
+        )
 
         # print(res)
 
