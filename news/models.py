8b88a9a0d1f7004cf95c583714d1daab1304d1d8
Jeremiah Lena <jeremiahlena13@gmail.com>
2023-02-12 01:36:54 +0100

    add entities field

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Interest(models.Model):
     name = models.CharField(max_length=100)
 
@@ -15,14 +16,18 @@ class News(models.Model):
     read_count = models.BigIntegerField(default=0)
     categories = models.ManyToManyField(to=Interest)
     time_added = models.DateTimeField(auto_now_add=True)
-    text_content = models.TextField(default = '')
+    text_content = models.TextField(default='')
 
     website_name = models.TextField(default='Unknown')
     website_favicon = models.URLField(
         default='https://media.istockphoto.com/vectors/news-vector-id918880270?k=20&m=918880270&s=612x612&w=0&h=bDcgr9jhiRYCPMUhVdLKD5ouIc5daM4qMcaPPapppQI=')
+    entities = models.TextField(default='')
+    """ 
+    the entities field is used by the machine learning model, entities is basically a big string separated by commas - to denote different entities
+    """
 
     def __str__(self):
         return self.title + ' (' + str(self.read_count) + ' reads)'
-    
+
     def serialize(self):
         return {'title': self.title, 'url': self.url, 'img': self.img, 'metadata': {'website': self.website_name, 'favicon': self.website_favicon}}
