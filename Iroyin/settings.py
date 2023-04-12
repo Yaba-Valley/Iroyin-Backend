bd52dd477f8327ba5a8b9f3f2cbc7d5a95c32b9f
Jeremiah Lena <jeremiahlena13@gmail.com>
2023-04-12 14:03:37 +0100

    fix

--- Iroyin/settings.py
+++ Iroyin/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'iroyin-backend-env.eba-wmgpq2d7.us-west-2.elasticbeanstalk.com', 'localhost', '.eu.ngrok.io']
--- news/views.py
+++ news/views.py
@@ -31,29 +31,29 @@ class Get_News(APIView):
 
         all_websites = list(Website.objects.all())
 
-        try:
-            recommended = Machine(request.user.id, news_per_page)
-            news_for_frontend = []
+        # try: 
+        recommended = Machine(request.user.id, news_per_page)
+        news_for_frontend = []
 
-            for news in recommended:
-                website = next(filter(lambda website: website.id == int(
-                    recommended['website_id']), all_websites), None)
-                metadata = website.generate_metadata()
-                metadata['time_added'] = website['time_added']
-                news_for_frontend.append(
-                    {'title': news['title'], 'url': news['url'], 'img': news['img'], 'metadata': metadata})
+        for news in recommended:
+            website = next(filter(lambda website: website.id == int(
+                recommended['website_id']), all_websites), None)
+            metadata = website.generate_metadata()
+            metadata['time_added'] = website['time_added']
+            news_for_frontend.append(
+                {'title': news['title'], 'url': news['url'], 'img': news['img'], 'metadata': metadata})
 
-            return JsonResponse({
-                'news': news_for_frontend,
-                'current_page': page_number,
-                'next_page': page_number + 1,
-                'per_page': news_per_page,
-                'total_pages': round(News.objects.count() / news_per_page)
-            })
+        return JsonResponse({
+            'news': news_for_frontend,
+            'current_page': page_number,
+            'next_page': page_number + 1,
+            'per_page': news_per_page,
+            'total_pages': round(News.objects.count() / news_per_page)
+        })
 
-        except Exception as e:
-            print(e)
-            return HttpResponse(f'<h1>THere is an error <hr /> {e}</h1>',  status=500)
+        # except Exception as e:
+            # print(e)
+            # return HttpResponse(f'<h1>THere is an error <hr /> {e}</h1>',  status=500)
 
 
 class Search_News(APIView):
